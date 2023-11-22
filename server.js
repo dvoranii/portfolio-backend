4a1550006070a6eff966d10ca973b620b041c9d6
Ildi Dvorani <dvoranii@ildidev.com>
2023-11-22 14:13:38 -0500

    fixed csurf middleware bug

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(limiter);
-app.use("/csrf-endpoint", csrfProtection);
 
 const csrfProtection = csrf({ cookie: true });
+app.use("/csrf-endpoint", csrfProtection);
 
 // const limiter = rateLimit({
 //   windowMs: 15 * 60 * 1000,
