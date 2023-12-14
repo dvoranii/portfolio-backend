84954e48908d56b80c19c61691ba9b1f6250bad4
Ildi Dvorani <dvoranii@ildidev.com>
2023-12-14 00:21:09 -0500

    added env var for heroku

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import routes from "./app.js";
 
 const app = routes;
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
