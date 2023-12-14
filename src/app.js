e5c5fd185c764050176926334990c50a8f9c5738
Ildi Dvorani <dvoranii@ildidev.com>
2023-12-14 09:39:55 -0500

    added procfile

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://ildidev.com"],
+    origin: ["http://localhost:5173", "https://www.ildidev.com"],
     credentials: true,
   })
 );
