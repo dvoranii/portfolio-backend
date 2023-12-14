df7b8e1402034b895b3482c6d9725a2bb7d70803
Ildi Dvorani <dvoranii@ildidev.com>
2023-12-14 11:35:14 -0500

    trying node-fetch cjs work-around

--- api/utils/verifyCaptcha.js
+++ api/utils/verifyCaptcha.js
@@ -1,4 +1,5 @@
-import fetch from "node-fetch";
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 const verifyRecaptcha = async (token) => {
   const secret = process.env.SECRET_KEY;
